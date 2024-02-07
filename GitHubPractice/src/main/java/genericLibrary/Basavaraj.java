78759577ceb6620bff06da4080e22e4b64e2a635
Basavaraj Awati <Basavaraj Awati@TYSS-Basavaraj-Awati>
2024-02-07 17:15:33 +0530

    First Commit

---
+++ GitHubPractice/src/main/java/genericLibrary/Basavaraj.java
@@ -0,0 +1,19 @@
+package genericLibrary;
+
+public class Basavaraj {
+	String name;
+	String empID;
+	long phNumber;
+
+	public void learn() {
+		System.out.println("We are learning GitHub");
+	}
+
+	public void work() {
+		System.out.println("We work for 9 hours");
+	}
+	public void sleep() {
+		System.out.println("We have to sleep for 6 hours");
+	}
+
+}
